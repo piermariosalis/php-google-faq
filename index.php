df4b19fc126b87b54b916ab028bd834572dc2ac2
PierMario <piermario.salis@aol.com>
2021-02-01 15:36:07 +0100

    Init

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <title>Google Policies</title>
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Roboto,arial', sans-serif;
+        }
+        
+
+
+        .header {
+            width: 100vw;
+        }
+
+        #site_logo {
+            width: 100%;
+            display: flex;
+            padding: 12px 20px;
+            
+        }
+
+        #site_logo img {
+            width: 73px;
+
+        }
+
+        #site_logo span 
+        {
+            margin-left: 12px;
+            align-self: center;
+            font-size: 22px;
+            color: #5f6368;
+        }
+
+        #site_nav {
+            width: 100vw;
+            display: flex;
+            padding: 0 20px;
+
+        }
+
+        #site_nav span {
+            padding: 5px 0;
+            margin: 0 15px;
+            font-size: 15px;
+            font-weight: bold;
+            color: #5f6368;
+
+        }
+
+        #site_nav #special {
+            color: #3367d6;
+            border-bottom: 2px solid #3367d6;
+
+        }
+    
+    
+    </style>
+</head>
+<body>
+    
+<div class="header">
+
+    <div id="site_logo">
+    <img src="Google-Logo.png" alt="">
+    <span>Privacy e termini</span>
+    </div>
+
+    
+
+</div>
+
+<nav id="site_nav">
+
+        <span>Introduzione</span>
+        <span>Norme sulla privacy</span>
+        <span>Termini di servizio</span>
+        <span>Tecnologie</span>
+        <span id="special">Domande Frequenti</span>
+
+    
+    
+</nav>
+
+
+
+
+
+
+<main>
+    <section id="one"></section>
+    <section id="one"></section>
+    <section id="one"></section>
+    <section id="one"></section>
+
+</main>
+
+
+
+</body>
+</html>
