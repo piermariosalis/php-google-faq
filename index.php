4c2674f37796977b313f6560b61f9b54f520d551
PierMario <piermario.salis@aol.com>
2021-02-01 17:04:15 +0100

    End

--- index.php
+++ index.php
@@ -22,11 +22,11 @@
 
         <p>Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.</p>
         
-        <p>Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.</p>
+        <p>Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a>Centro Google per la sicurezza online.</a></p>
         
-        <p>Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.</p>",
-        "answ_three" => "<p>I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.</p>",
-        "answ_four" => "<p>In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</p>"
+        <p><a>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.</p>",
+        "answ_three" => "<p>I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a>fai clic qui.</a> Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a>visitare la nostra pagina di assistenza per avere ulteriori informazioni.</a></p>",
+        "answ_four" => "<p>In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a>URL referrer</a>. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a>qui.</a> Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</p>"
     ];
 
 
@@ -37,11 +37,18 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+        integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+        crossorigin="anonymous" />
     <title>Google Policies</title>
 
     <style>
@@ -49,9 +56,10 @@
             margin: 0;
             padding: 0;
             box-sizing: border-box;
-            font-family: 'Roboto,arial', sans-serif;
+            font-family: 'Open-Sans', sans-serif;
+            font-weight: 500;
         }
-        
+
 
 
         .header {
@@ -62,7 +70,7 @@
             width: 100%;
             display: flex;
             padding: 12px 20px;
-            
+
         }
 
         #site_logo img {
@@ -70,8 +78,7 @@
 
         }
 
-        #site_logo span 
-        {
+        #site_logo span {
             margin-left: 12px;
             align-self: center;
             font-size: 22px;
@@ -112,51 +119,80 @@
             margin: auto;
             padding: 26px 0 38px 0;
 
-        
+
         }
 
         main section h2 {
             font-weight: 600;
             font-size: 24px;
             color: #3c4043;
+            font-family: 'Open-Sans', sans-serif;
         }
 
-        main section h2 {
-            font-weight: 600;
-            font-size: 24px;
-            color: #3c4043;
-        }
 
-        section p  {
+        section p {
             padding: 22px 0;
             font-size: 14px;
             color: #3c4043;
             font-weight: 400;
             line-height: 1.8;
-            font-family: Roboto,arial,sans-serif;
+            font-family: Roboto, arial, sans-serif;
         }
-         section p a {
-             color: #2962ff;
-         }
 
-    
-    
+        section p a {
+            color: #2962ff;
+        }
+
+        footer {
+            width: 100vw;
+            background: #F5F5F5;
+            display: flex;
+            padding: 18px 122px;
+
+
+        }
+
+
+        #footer_content {
+            display: flex;
+
+            width: 978px;
+            margin: auto;
+        }
+
+        #footer_left,
+        #footer_right {
+            width: calc(100% / 2);
+            font-size: 11px;
+        }
+
+        #footer_left span>a {
+            text-decoration: none;
+            color: #3D3D3DCC;
+            margin: 0 6px;
+        }
+
+        #footer_right {
+            display: flex;
+            justify-content: flex-end;
+        }
     </style>
 </head>
-<body>
-    
-<div class="header">
 
-    <div id="site_logo">
-    <img src="Google-Logo.png" alt="">
-    <span>Privacy e termini</span>
+<body>
+
+    <div class="header">
+
+        <div id="site_logo">
+            <img src="Google-Logo.png" alt="">
+            <span>Privacy e termini</span>
+        </div>
+
+
+
     </div>
 
-    
-
-</div>
-
-<nav id="site_nav">
+    <nav id="site_nav">
 
         <span>Introduzione</span>
         <span>Norme sulla privacy</span>
@@ -164,40 +200,78 @@
         <span>Tecnologie</span>
         <span id="special">Domande Frequenti</span>
 
-    
-    
-</nav>
+
+
+    </nav>
 
 
 
 
 
 
-<main>
-    <section id="one">
+    <main>
+        <section id="one">
 
-   <h2><?php echo $questions["quest_one"] ?></h2>
-   <div><?php echo $answers["answ_one"] ?></div>
-    
-    
-    </section>
-    <section id="one">
-    <h2><?php echo $questions["quest_two"] ?></h2>
-   <div><?php echo $answers["answ_two"] ?></div>
-    </section>
-    <section id="one">
-    <h2><?php echo $questions["quest_three"] ?></h2>
-   <div><?php echo $answers["answ_three"] ?></div>
-    
-    </section>
-    <section id="one">
-    <h2><?php echo $questions["quest_four"] ?></h2>
-   <div><?php echo $answers["answ_four"] ?></div>
-    </section>
-
-</main>
+            <h2><?php echo $questions["quest_one"] ?></h2>
+            <div><?php echo $answers["answ_one"] ?></div>
 
 
+        </section>
+        <section id="one">
+            <h2><?php echo $questions["quest_two"] ?></h2>
+            <div><?php echo $answers["answ_two"] ?></div>
+        </section>
+        <section id="one">
+            <h2><?php echo $questions["quest_three"] ?></h2>
+            <div><?php echo $answers["answ_three"] ?></div>
+
+        </section>
+        <section id="one">
+            <h2><?php echo $questions["quest_four"] ?></h2>
+            <div><?php echo $answers["answ_four"] ?></div>
+        </section>
+
+    </main>
+
+    <footer>
+
+        <div id="footer_content">
+
+            <div id="footer_left">
+
+                <span><a href="">Google</a> </span>
+                <span><a href="">Tutto su Google</a></span>
+                <span><a href="">Privacy</a></span>
+                <span><a href="">Termini</a></span>
+
+
+            </div>
+
+            <div id="footer_right">
+                <i style="color: blue; font-size: 14px; margin: 0 8px; line-height: 19px;"
+                    class="fas fa-comment-alt"></i>
+                <select style="width: 180px;" name="countries" id="countries">
+                    <option value="Italia">Italiano</option>
+                    <option value="Lorem">Lorem</option>
+                    <option value="Lorem">Lorem</option>
+                    <option value="Lorem">Lorem</option>
+                    <option value="Lorem">Lorem</option>
+                    <option value="Lorem">Lorem</option>
+                    <option value="Lorem">Lorem</option>
+                    <option value="Lorem">Lorem</option>
+
+
+                </select>
+            </div>
+
+
+
+        </div>
+
+
+
+    </footer>
 
 </body>
+
 </html>
